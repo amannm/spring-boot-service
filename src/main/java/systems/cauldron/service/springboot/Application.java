7d7bc7f00a10c4b27f6969a0476238ff7ce5b3e1
Amann Malik <amannmalik@gmail.com>
2018-09-16 11:17:27 -0500

    make sure JUL logging is routed properly to Log4J2

--- src/main/java/systems/cauldron/service/springboot/Application.java
+++ src/main/java/systems/cauldron/service/springboot/Application.java
@@ -6,6 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
 
+    static {
+        System.setProperty("java.util.logging.manager", "org.apache.logging.log4j.jul.LogManager");
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
