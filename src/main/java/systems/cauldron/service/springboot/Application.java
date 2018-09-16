4c2943e484d7c7f9ece697379c9a06b9f8092940
Amann Malik <amannmalik@gmail.com>
2018-09-16 12:41:01 -0500

    remove redundant annotation

--- src/main/java/systems/cauldron/service/springboot/Application.java
+++ src/main/java/systems/cauldron/service/springboot/Application.java
@@ -1,11 +1,9 @@
 package systems.cauldron.service.springboot;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableAutoConfiguration
 public class Application {
 
     static {
