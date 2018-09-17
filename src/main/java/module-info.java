5b91dc491f5d189be8b7814eb0a305acfaea1071
Amann Malik <amannmalik@gmail.com>
2018-09-16 20:12:13 -0500

    removing whitespace overclocks my cpu mang

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,14 +1,10 @@
 open module systems.cauldron.service.springboot {
-
+    exports systems.cauldron.service.springboot;
     requires java.sql;
-
     requires org.apache.logging.log4j;
-
     requires spring.context;
     requires spring.beans;
     requires spring.web;
-
     requires spring.boot;
     requires spring.boot.autoconfigure;
-
 }
